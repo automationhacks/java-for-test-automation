896d548803d5f091f7152ac9392f965c3b723ae0
automationhacks <gaurav.singh55@gmail.com>
2024-04-11 14:49:06 +0530

    Add files

--- java-for-test-automation/src/main/java/io/automationhacks/Main.java
+++
@@ -1,7 +0,0 @@
-package io.automationhacks;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- java-for-test-automation/src/test/java/io/automationhacks/java9/_02_private_methods_in_interface/PrivateMethodsInInterfaceImpl.java
+++
@@ -1,7 +0,0 @@
-package io.automationhacks.java9._02_private_methods_in_interface;
-
-public class PrivateMethodsInInterfaceImpl implements PrivateMethodsInInterface {
-    public static void main(String[] args) {
-        new PrivateMethodsInInterfaceImpl().check();
-    }
-}
