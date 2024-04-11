e70e931eb0e660aababb07ab1d9e5b21cf0bad31
automationhacks <gaurav.singh55@gmail.com>
2024-04-11 20:53:22 +0530

    Commit files and fixed broken test on String methods

--- src/test/java/io/automationhacks/java11/_02_string_methods/StringMethods.java
+++ src/test/java/io/automationhacks/java11/_02_string_methods/StringMethods.java
@@ -1,16 +1,14 @@
 package io.automationhacks.java11._02_string_methods;
 
-import org.testng.annotations.Test;
-
 import static com.google.common.truth.Truth.assertWithMessage;
+
+import org.testng.annotations.Test;
 
 public class StringMethods {
     @Test
     public void testStringMethods() {
         String str = " AutomationHacks ";
-        System.out.println("Length: " + str.length());
-
-        assertWithMessage("Length does not match").that(str.length()).isEqualTo(14);
+        assertWithMessage("Length does not match").that(str.length()).isEqualTo(17);
         assertWithMessage("Is not blank").that(str.isBlank()).isFalse();
         assertWithMessage("Strip does not match").that(str.strip()).isEqualTo("AutomationHacks");
         assertWithMessage("Strip Leading does not match")
