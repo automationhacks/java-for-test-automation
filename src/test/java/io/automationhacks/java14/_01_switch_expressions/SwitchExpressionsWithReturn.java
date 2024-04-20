13c901f6fd8eef5dd40680f2a911fb0fdfa7d46e
automationhacks <automationhacks@gmail.com>
2024-04-20 20:01:40 +0530

    Updated SwitchExpressionsWithReturn example to add how a block of code can look like along with yield statement

--- src/test/java/io/automationhacks/java14/_01_switch_expressions/SwitchExpressionsWithReturn.java
+++ src/test/java/io/automationhacks/java14/_01_switch_expressions/SwitchExpressionsWithReturn.java
@@ -1,9 +1,8 @@
 package io.automationhacks.java14._01_switch_expressions;
 
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import org.testng.annotations.Test;
-
-import static com.google.common.truth.Truth.assertWithMessage;
 
 public class SwitchExpressionsWithReturn {
     @Test
@@ -16,13 +15,17 @@ public class SwitchExpressionsWithReturn {
                 .isEqualTo("Client Error");
     }
 
-    // In Java 13, this return was possible with yield keyword which was
-    // later removed and we can directly return from switch expression.
+    // In case switch has to execute a block of code we can use yield keyword to return the value
+    // from
+    // the block
     public String switchExpressionWithReturn(int code) {
         return switch (code) {
             case 200, 201, 202 -> "Success";
             case 400, 404 -> "Client Error";
-            case 500, 503 -> "Server Error";
+            case 500, 503 -> {
+                String errorType = "Server Error";
+                yield errorType;
+            }
             default -> "Unknown";
         };
     }
