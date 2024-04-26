2cff16c9bfc277b9eff3e40e7c486ad57a25b1c4
automationhacks <automationhacks@gmail.com>
2024-04-26 08:17:08 +0530

    Java 8: Interface default with and without override, and with the use of static

--- src/test/java/io/automationhacks/java8/_01_interface_default_static_methods/Vehicle.java
+++ src/test/java/io/automationhacks/java8/_01_interface_default_static_methods/Vehicle.java
@@ -2,6 +2,7 @@ package io.automationhacks.java8._01_interface_default_static_methods;
 
 public interface Vehicle {
     // static cannot be overridden in the implementing class
+    // it is only available through and inside an interface
     static String producer() {
         return "Toyota";
     }
--- src/test/java/io/automationhacks/java8/_01_interface_default_static_methods/VehicleTest.java
+++ src/test/java/io/automationhacks/java8/_01_interface_default_static_methods/VehicleTest.java
@@ -11,6 +11,8 @@ class TataNexon implements Vehicle {
     }
 }
 
+class ToyotaEtios implements Vehicle {}
+
 public class VehicleTest {
     @Test
     public void testInterfaceStaticInVehicle() {
@@ -21,6 +23,13 @@ public class VehicleTest {
 
     @Test
     public void testInterfaceDefaultInVehicle() {
+        var toyotaEtios = new ToyotaEtios();
+        String overview = toyotaEtios.getOverview();
+        assertWithMessage("Invalid vehicle overview").that(overview).isEqualTo("SUV from Toyota");
+    }
+
+    @Test
+    public void testInterfaceDefaultOverrideInVehicle() {
         var tataNexon = new TataNexon();
         String overview = tataNexon.getOverview();
         assertWithMessage("Invalid vehicle overview")
