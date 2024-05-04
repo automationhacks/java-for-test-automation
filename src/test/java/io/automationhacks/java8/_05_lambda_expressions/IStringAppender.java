a9b59a370821169dc2a08e23f2f37beee79e464c
automationhacks <automationhacks@gmail.com>
2024-05-04 08:50:40 +0530

    Added a comment to explain what functional interface means

--- src/test/java/io/automationhacks/java8/_05_lambda_expressions/IStringAppender.java
+++ src/test/java/io/automationhacks/java8/_05_lambda_expressions/IStringAppender.java
@@ -1,5 +1,7 @@
 package io.automationhacks.java8._05_lambda_expressions;
 
+// Adding @FunctionalInterface prevents someone else to add another abstract method
+// by mistake and ensures this interface can ONLY have one method
 @FunctionalInterface
 public interface IStringAppender {
     String method(String string);
